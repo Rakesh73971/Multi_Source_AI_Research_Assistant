1c2d2176644f08d83d3afa16021db4ef7cd3f231
Rakesh N <nraki522@gmail.com>
2026-05-21 19:36:57 +0530

    imported the new routers

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import oauth,user,research_session
+from .routers import oauth,user,research_session,source
 from . import models
 from app.db.database import engine
 
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(oauth.router)
 app.include_router(user.router)
 app.include_router(research_session.router)
+app.include_router(source.router)
