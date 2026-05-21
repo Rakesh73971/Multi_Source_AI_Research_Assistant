f2fdb6f8526a0db1cf49c37dc9220e42d0ae4a25
Rakesh N <nraki522@gmail.com>
2026-05-21 15:37:10 +0530

    fixed the role based error

--- app/models/user.py
+++ app/models/user.py
@@ -16,7 +16,7 @@ class User(Base):
     full_name = Column(String,nullable=False)
     email = Column(String,unique=True,nullable=False)
     password = Column(String,nullable=False)
-    role = Column(SAEnum(UserRole, values_callable=lambda x: [e.value for e in x]),default=UserRole.USER, nullable=False)
+    role = Column(SAEnum(UserRole, values_callable=lambda x: [e.value for e in x]),default=UserRole.user, nullable=False)
     is_active = Column(Boolean,server_default='True',nullable=False)
     created = Column(TIMESTAMP(timezone=True),server_default=text('now()'),nullable=False)
     sessions = relationship('ResearchSession',back_populates='user')
