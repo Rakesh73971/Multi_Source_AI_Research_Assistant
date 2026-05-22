5fcdadd7ea7794c05a286f5c50abdd2e3ae0b67e
Rakesh N <nraki522@gmail.com>
2026-05-22 14:20:02 +0530

    imported the conversion message router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import oauth,user,research_session,source
+from .routers import oauth,user,research_session,source,conversion_message
 from . import models
 from app.db.database import engine
 
@@ -11,3 +11,4 @@ app.include_router(oauth.router)
 app.include_router(user.router)
 app.include_router(research_session.router)
 app.include_router(source.router)
+app.include_router(conversion_message.router)
