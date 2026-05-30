711d1c7866f34c3a7418b2f965fed97bacdb3d78
Rakesh N <nraki522@gmail.com>
2026-05-30 12:13:17 +0530

    tested the user api methods

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,3 +11,23 @@ def test_create_user(client):
 def test_get_users(authorized_access):
     responses = authorized_access.get("/users/")
     assert responses.status_code == 200
+
+
+def test_get_user(authorized_access,test_user):
+    user_id = test_user["id"]
+    response = authorized_access.get(f"/users/{user_id}")
+    assert response.status_code == 200
+
+def test_update_user(authorized_access,test_user):
+    user_data = {
+        "full_name":"Gukesh"
+    }
+    response = authorized_access.patch(f"/users/{test_user['id']}",json=user_data)
+
+    assert response.status_code == 200
+    assert response.json()["full_name"] == "Gukesh"
+
+def test_delete_user(authorized_access,test_user):
+    user_id = test_user["id"]
+    response = authorized_access.delete(f"/users/{user_id}")
+    assert response.status_code == 204
